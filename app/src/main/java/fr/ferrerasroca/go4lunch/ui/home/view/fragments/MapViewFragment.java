9a2f88e814b2372f04bc2efa90351110536e8224
Bryan FR <bryan.ferreras@gmail.com>
2021-06-08 11:10:41 +0200

    Adding import

--- app/src/main/java/fr/ferrerasroca/go4lunch/ui/home/view/fragments/MapViewFragment.java
+++ app/src/main/java/fr/ferrerasroca/go4lunch/ui/home/view/fragments/MapViewFragment.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.Observer;
 
 import com.google.android.gms.location.LocationCallback;
