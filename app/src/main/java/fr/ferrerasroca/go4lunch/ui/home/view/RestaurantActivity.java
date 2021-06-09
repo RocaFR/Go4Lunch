b8c89a724e439906c385dd0e88429fbcb02091ed
Bryan FR <bryan.ferreras@gmail.com>
2021-06-09 11:45:07 +0200

    UI implementation

---
+++ app/src/main/java/fr/ferrerasroca/go4lunch/ui/home/view/RestaurantActivity.java
@@ -0,0 +1,16 @@
+package fr.ferrerasroca.go4lunch.ui.home.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fr.ferrerasroca.go4lunch.R;
+
+public class RestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant);
+    }
+}
